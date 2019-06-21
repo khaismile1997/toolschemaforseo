22c83a89682d3c0f73338ad3afaad6755c815ac2
Khai Nguyen <khaismile1997@gmail.com>
2019-06-21 22:25:18 +0700

    heroku

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "";
-$db_name = "schemagtv";
+$db_host = "ec2-23-21-129-125.compute-1.amazonaws.com:5432";
+$db_user = "abjhmdleykitid";
+$db_password = "6f4e5c6a3d2e404e0c8862f0e49fc41895f0619ae530378569d67c6e05cdaecc";
+$db_name = "d6moue1f872k8i";
 
 // Create connection
 $db = mysqli_connect($db_host, $db_user, $db_password, $db_name);
